5c98616db43bc1a21d96ce6afe960bfc06ec5140
gabrielalves-data <gabrielda2.work@gmail.com>
2025-11-09 01:19:25 +0000

    chore: make app port configurable via PORT env for Render

--- telco_customer_churn_analysis/src/application.py
+++ telco_customer_churn_analysis/src/application.py
@@ -1031,4 +1031,5 @@ def predict_xai():
 
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port, debug=False)
